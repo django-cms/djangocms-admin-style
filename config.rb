146e232e5979982aec7b0d67c0457edaa17c2298
FinalAngel <angelo.dini@divio.ch>
2013-11-14 17:41:39 +0100

    add compass config file

---
+++ config.rb
@@ -0,0 +1,14 @@
+# COMPASS SETTINGS
+
+http_path = "" # use if other than /
+sass_dir = "djangocms_admin_style/sass"
+css_dir = "djangocms_admin_style/static/djangocms_admin_style/css"
+
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
