f0e0c905c9a63024248643085871f49e6986c5cc
Loriana Indelicato <loriana_indelicato@hotmail.com>
2016-04-21 13:57:27 +0200

    typo

--- djangocms_admin_style/static/djangocms_admin_style/js/base-admin.js
+++ djangocms_admin_style/static/djangocms_admin_style/js/base-admin.js
@@ -71,7 +71,7 @@
                 if ($('#changelist-form').find('.actions').length) {
                     $('#toolbar').addClass('actions-visible');
                 }
-                // adds class to toplinks list is empty to remove unnecessary space
+                // adds class to toplinks if list is empty to remove unnecessary space
                 if (toplinks.children().length === 0 && toplinks.parent().hasClass('xfull')) {
                     toplinks.parent().addClass('hidden');
                 }
