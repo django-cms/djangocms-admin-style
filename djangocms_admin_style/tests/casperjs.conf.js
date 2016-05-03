a9197bbe58e27baeb494c7c859ad26b957f9c189
Loriana Indelicato <loriana_indelicato@hotmail.com>
2016-05-03 16:00:44 +0200

    updates casper config with phantomcss

--- djangocms_admin_style/tests/casperjs.conf.js
+++ djangocms_admin_style/tests/casperjs.conf.js
@@ -1,4 +1,5 @@
 'use strict';
+var phantomcss = require('phantomcss');
 
 // #############################################################################
 // CasperJS options
@@ -7,6 +8,7 @@ module.exports = {
     init: function () {
         this.viewportSize();
         this.timeout(20000);
+        this.phantomcss();
     },
 
     viewportSize: function (width, height) {
@@ -19,6 +21,35 @@ module.exports = {
             width: viewportWidth,
             height: viewportHeight
         };
+    },
+    phantomcss: function () {
+        phantomcss.init({
+            libraryRoot: './node_modules/phantomcss',
+            screenshotRoot: './screenshots/reference',
+            failedComparisonsRoot: './screenshots/failures',
+            comparisonResultRoot: './screenshots/results',
+            cleanupComparisonImages: true,
+            mismatchTolerance: 0.05,
+            onPass: function (test) {
+                casper.test.pass('No changes found for screenshot ' + test.filename);
+            },
+            onComplete: function (allTests, noOfFails, noOfErrors) {
+                allTests.forEach(function (test) {
+                    if (test.fail) {
+                        console.log(test.filename, test.mismatch);
+                    }
+                });
+            },
+            outputSettings: {
+                errorColor: {
+                    red: 255,
+                    green: 0,
+                    blue: 0
+                },
+                errorType: 'movement',
+                transparency: 0.3
+            }
+        });
     },
 
     timeout: function (timeout) {
