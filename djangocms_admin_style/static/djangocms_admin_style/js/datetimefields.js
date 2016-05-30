4bea3ed9e18fa89482ed08c1ef582336bf0980da
Vadim Sikora <vadim.sikora@divio.ch>
2016-05-30 17:11:47 +0200

    == -> ===

--- djangocms_admin_style/static/djangocms_admin_style/js/datetimefields.js
+++ djangocms_admin_style/static/djangocms_admin_style/js/datetimefields.js
@@ -7,7 +7,7 @@
      * @returns {void}
      */
     function ready(fn) {
-        if (document.readyState != 'loading'){
+        if (document.readyState !== 'loading'){
             fn();
         } else {
             document.addEventListener('DOMContentLoaded', fn);
