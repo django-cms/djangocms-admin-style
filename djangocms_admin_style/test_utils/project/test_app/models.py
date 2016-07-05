1ba3924b9f0e199052f10c37f26e912880850230
Ihor Kucher <igor.k.843@gmail.com>
2016-07-05 15:05:24 +0300

    integration_tests:models: change fields ordering, add comment

--- djangocms_admin_style/test_utils/project/test_app/models.py
+++ djangocms_admin_style/test_utils/project/test_app/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 
 class GenericFields(models.Model):
+    """
+    Model for integration test with all generic fields.
+    Order of fields is important.
+    """
     char_field = models.CharField(max_length=255)
     integer_field = models.IntegerField()
     boolean_field = models.BooleanField()
@@ -14,13 +18,14 @@ class GenericFields(models.Model):
     decimal_field = models.DecimalField(max_digits=20, decimal_places=5)
     float_field = models.FloatField()
 
-    email_field = models.EmailField()
     file_field = models.FileField()
     file_path_field = models.FilePathField()
-    generic_ip_address_field = models.GenericIPAddressField()
-    slug_field = models.SlugField()
     text_field = models.TextField()
+    slug_field = models.SlugField()
     url_field = models.URLField()
+    email_field = models.EmailField()
+    generic_ip_address_field = models.GenericIPAddressField()
+
     foreign_key = models.ForeignKey('RelatedModel', related_name='+')
     many_to_many_field = models.ManyToManyField('RelatedModel', related_name='+')
     one_to_one_field = models.OneToOneField('RelatedModel', related_name='+')
