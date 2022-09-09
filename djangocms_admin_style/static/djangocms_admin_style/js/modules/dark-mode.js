a66a7fe286f177ae0ac4201bb514f9851c67ef55
Fabian Braun <fsbraun@gmx.de>
2022-09-09 18:12:15 +0200

    add: Reflect individual color scheme setting of django CMS 3.11 on admin site (#466)
    * Add:		Dark color scheme
    
    * Fix:		Remove !important statements preventing forms to implement dark mode
    
    * Fix:		Final color values
    
    * Add:		Changelog entry
    Fix:		Announce dark mode to the browser for scrollbar coloring
    
    * Fix:		Disappearing shadows
    Fix:		deprecation warning for /
    
    * Fix:		Replace color literals by variables
    Fix:		Leave more shadows dark in dark mode
    
    * Add:		configurable color scheme
    
    * Fix:		Propagate dark mode to iframes
    
    * Fix:		color scheme switch as css variables
    
    * Fix:		Minified css
    
    * Fix:		Cascade settings into iframes of iframes (e.g., cms plugins inside ckeditor)
    
    * Update the comments
    
    * Fix:		Better color scheme selection on admin site

--- djangocms_admin_style/static/djangocms_admin_style/js/modules/dark-mode.js
+++ djangocms_admin_style/static/djangocms_admin_style/js/modules/dark-mode.js
@@ -6,22 +6,27 @@
  */
 function darkModeSettings() {
     if (!document.documentElement.dataset.colorScheme) {
-        var colorScheme = 'light'; // Default mode
+        var colorScheme = 'auto'; // Default mode
         var cms_window = window;
 
         while (cms_window.parent !== cms_window) {
             cms_window = cms_window.parent;
         }
-        if (cms_window.CMS && cms_window.CMS.config) {
-            if (cms_window.CMS.settings.color_scheme) {
+        if (cms_window.CMS) {
+            if (cms_window.CMS.settings && cms_window.CMS.settings.color_scheme) {
                 // Use color_scheme from settings.py
                 colorScheme = cms_window.CMS.settings.color_scheme;
-            } else if (cms_window.CMS.config.color_scheme) {
+            } else if (cms_window.CMS.config && cms_window.CMS.config.color_scheme) {
                 // If overwritten by config use config. This is the toggle button
                 colorScheme = cms_window.CMS.config.color_scheme;
             }
+        } else if (window.localStorage) {
+            // CMS not loaded: set color scheme for admin site according to settings
+            colorScheme = JSON.parse(localStorage.getItem('cms_cookie') || '{}').color_scheme;
         }
-        if (colorScheme !== 'auto') {
+        if (colorScheme === 'auto') {
+            delete document.documentElement.dataset.colorScheme;
+        } else {
             document.documentElement.dataset.colorScheme = colorScheme;
         }
     }
