79adcaaa06f4ee592161bcdb2853b4647840470f
Fabian Braun <fsbraun@gmx.de>
2022-09-13 18:47:52 +0200

    Fix: Reduce complexity of dark-mode.js

--- djangocms_admin_style/static/djangocms_admin_style/js/modules/dark-mode.js
+++ djangocms_admin_style/static/djangocms_admin_style/js/modules/dark-mode.js
@@ -4,22 +4,33 @@
  * @function darkModeSettings
  * @returns {void}
  */
+
+function getTopWindow() {
+    var cms_window = window;
+
+    while (cms_window.parent !== cms_window) {
+        cms_window = cms_window.parent;
+    }
+    return cms_window;
+}
+
+function getColorSchemFromSettings(CMS) {
+    if (CMS.settings && CMS.settings.color_scheme) {
+        // Use color_scheme from settings.py
+        colorScheme = CMS.settings.color_scheme;
+    } else if (CMS.config && CMS.config.color_scheme) {
+        // If overwritten by config use config. This is the toggle button
+        colorScheme = CMS.config.color_scheme;
+    }
+}
+
 function darkModeSettings() {
     if (!document.documentElement.dataset.colorScheme) {
         var colorScheme = 'auto'; // Default mode
-        var cms_window = window;
+        var cms_window = getTopWindow();
 
-        while (cms_window.parent !== cms_window) {
-            cms_window = cms_window.parent;
-        }
         if (cms_window.CMS) {
-            if (cms_window.CMS.settings && cms_window.CMS.settings.color_scheme) {
-                // Use color_scheme from settings.py
-                colorScheme = cms_window.CMS.settings.color_scheme;
-            } else if (cms_window.CMS.config && cms_window.CMS.config.color_scheme) {
-                // If overwritten by config use config. This is the toggle button
-                colorScheme = cms_window.CMS.config.color_scheme;
-            }
+            colorScheme = getColorSchemeFromSettings(cms_window.CMS);
         } else if (window.localStorage) {
             // CMS not loaded: set color scheme for admin site according to settings
             colorScheme = JSON.parse(localStorage.getItem('cms_cookie') || '{}').color_scheme;
