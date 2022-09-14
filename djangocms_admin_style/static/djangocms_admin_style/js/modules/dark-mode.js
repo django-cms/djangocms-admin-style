ad3d397aa5601e4be79f42dfa459b656bf33f64a
Fabian Braun <fsbraun@gmx.de>
2022-09-14 08:54:09 +0200

    fix:  set css variables in django cms v4, filer layout (#468)
    Improved interoperationality w/o django-filer
    Missing css variables on djangocms v4
    Reduce complexity of dark-mode.js

--- djangocms_admin_style/static/djangocms_admin_style/js/modules/dark-mode.js
+++ djangocms_admin_style/static/djangocms_admin_style/js/modules/dark-mode.js
@@ -1,4 +1,39 @@
 /**
+ * Get top most window (from iframe)
+ *
+ * @function getTopWindow
+ * @returns {window}
+ */
+function getTopWindow() {
+    var cms_window = window;
+
+    while (cms_window.parent !== cms_window) {
+        cms_window = cms_window.parent;
+    }
+    return cms_window;
+}
+
+/**
+ * Get color scheme from CMS settings
+ *
+ * @function getColorSchemeFromSettings
+ * @param {object} CMS
+ * @returns {string}
+ */
+function getColorSchemeFromSettings(CMS) {
+    var colorScheme;
+
+    if (CMS.settings && CMS.settings.color_scheme) {
+        // Use color_scheme from settings.py
+        colorScheme = CMS.settings.color_scheme;
+    } else if (CMS.config && CMS.config.color_scheme) {
+        // If overwritten by config use config. This is the toggle button
+        colorScheme = CMS.config.color_scheme;
+    }
+    return colorScheme;
+}
+
+/**
  * Get color scheme from parent document (if in iframe) else set to white
  *
  * @function darkModeSettings
@@ -7,19 +42,10 @@
 function darkModeSettings() {
     if (!document.documentElement.dataset.colorScheme) {
         var colorScheme = 'auto'; // Default mode
-        var cms_window = window;
+        var cms_window = getTopWindow();
 
-        while (cms_window.parent !== cms_window) {
-            cms_window = cms_window.parent;
-        }
         if (cms_window.CMS) {
-            if (cms_window.CMS.settings && cms_window.CMS.settings.color_scheme) {
-                // Use color_scheme from settings.py
-                colorScheme = cms_window.CMS.settings.color_scheme;
-            } else if (cms_window.CMS.config && cms_window.CMS.config.color_scheme) {
-                // If overwritten by config use config. This is the toggle button
-                colorScheme = cms_window.CMS.config.color_scheme;
-            }
+            colorScheme = getColorSchemeFromSettings(cms_window.CMS);
         } else if (window.localStorage) {
             // CMS not loaded: set color scheme for admin site according to settings
             colorScheme = JSON.parse(localStorage.getItem('cms_cookie') || '{}').color_scheme;
