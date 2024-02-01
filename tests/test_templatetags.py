2641bf32f51513c4e5f66018278318f7f93d4a4b
raffaella <suardini.raffaella@gmail.com>
2024-02-01 18:11:03 +0100

    fix ruff rule I001

--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -1,7 +1,6 @@
+import cms
 from django.test import TestCase
 from django.test.client import RequestFactory
-
-import cms
 
 
 class Object:
@@ -16,14 +15,17 @@ class TemplateTagsTestCase(TestCase):
         from djangocms_admin_style.templatetags.admin_style_tags import (
             current_site_name,
         )
+
         site_name = current_site_name(dict())
         self.assertEqual(site_name, "example.com")  # Default name
 
-        with self.modify_settings(INSTALLED_APPS={
-            'remove': [
-                'django.contrib.sites',
-            ],
-        }):
+        with self.modify_settings(
+            INSTALLED_APPS={
+                "remove": [
+                    "django.contrib.sites",
+                ],
+            }
+        ):
             site_name = current_site_name(dict())
             self.assertEqual(site_name, "my site")  # Generic name
 
@@ -31,10 +33,15 @@ class TemplateTagsTestCase(TestCase):
         from djangocms_admin_style.templatetags.admin_style_tags import (
             render_update_notification,
         )
-        self.assertEqual(render_update_notification(dict()), "")  # No update notification
+
+        self.assertEqual(
+            render_update_notification(dict()), ""
+        )  # No update notification
 
         request = self.request_factory.get("/")
         request.resolver_match = Object()
         request.resolver_match.url_name = "index"
-        self.assertIn(f'<meta name="djangocms_version" content="{cms.__version__}">',
-                      render_update_notification(dict(request=request)))
+        self.assertIn(
+            f'<meta name="djangocms_version" content="{cms.__version__}">',
+            render_update_notification(dict(request=request)),
+        )
